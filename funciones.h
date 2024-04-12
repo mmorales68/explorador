fdeb353a6d3d83a7749d906dc3c868763201a5da
mmorales68 <miguelm4032@gmail.com>
2024-04-11 21:23:54 -0600

    Inicializacion del repositorio

---
+++ funciones.h
@@ -0,0 +1,16 @@
+//
+// Created by mmora on 11/04/2024.
+//
+
+#ifndef EXPLORADOR_FUNCIONES_H
+#define EXPLORADOR_FUNCIONES_H
+
+#endif //EXPLORADOR_FUNCIONES_H
+
+#include "iostream"
+using namespace std;
+
+
+void hola(){
+    cout<<"Hola, soy una funcion en una biblioteca externa";
+}
---
+++ main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by mmora on 11/04/2024.
+//
+
+#include "iostream"
+#include "funciones.h"
+
+using namespace std;
+
+int main(){
+    hola();
+    return 0;
+}
