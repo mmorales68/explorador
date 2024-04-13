d44ef26b133c69a8ff09f7156bdaba7a62e0c4e6
mmorales68 <miguelm4032@gmail.com>
2024-04-12 20:08:19 -0600

    Se agregan las funcionalidades principales del programa, cuenta con error en dos de ellas

--- funciones.h
+++ funciones.h
@@ -4,13 +4,107 @@
 
 #ifndef EXPLORADOR_FUNCIONES_H
 #define EXPLORADOR_FUNCIONES_H
+#endif
 
-#endif //EXPLORADOR_FUNCIONES_H
+#include <iostream>
+#include <filesystem>
+#include <string>
 
-#include "iostream"
 using namespace std;
+namespace fs = std::filesystem;
 
-
-void hola(){
-    cout<<"Hola, soy una funcion en una biblioteca externa";
+//mostrar el contenido de un directorio
+void mostrarContenido(const string& path){
+    try{
+        cout << "Contenido de " << path << ":\n";
+        for (const auto &entry: fs::directory_iterator(path)){
+            cout << entry.path().filename().string() << "\n";
+        }
+    }catch(runtime_error& e){
+        cout << "Error: " << e.what() << endl;
+    }
 }
+
+//validacion de directorio
+bool validarDirectorio(const string& path){
+    if(fs::is_directory(path)){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+//copiar un archivo
+void copiarArchivo(const string& source, const string& destination){
+    try{
+        fs::copy_file(source, destination, fs::copy_options::overwrite_existing);
+        cout << "Archivo copiado exitosamente.\n";
+    }catch(exception& e){
+        cout << "Error: " << e.what() << endl;
+    }
+}
+
+//renombrar un archivo
+void renombrarArchivo(const string& oldName, const string& newName){
+    try{
+        fs::rename(oldName, newName);
+        cout << "Archivo renombrado exitosamente.\n";
+    }catch(exception& e){
+        cout << "Error: " << e.what() << endl;
+    }
+}
+
+//mover un archivo
+void moverArchivo(const string& source, const string& destination){
+    try{
+        fs::rename(source, destination);
+        cout << "Archivo movido exitosamente.\n";
+    }catch(exception& e){
+        cout << "Error: " << e.what() << endl;
+    }
+}
+
+//borrar un archivo
+void borrarArchivo(const string& filePath){
+    try{
+        fs::remove(filePath);
+        cout << "Archivo borrado exitosamente.\n";
+    }catch(exception& e){
+        cout << "Error: " << e.what() << endl;
+    }
+}
+
+//renombrar una carpeta
+void renombrarCarpeta(const string& oldName, const string& newName){
+    try{
+        fs::rename(oldName, newName);
+        cout << "Carpeta renombrada exitosamente.\n";
+    }catch(exception& e){
+        cout << "Error: " << e.what() << endl;
+    }
+}
+
+//crear una carpeta
+void crearCarpeta(const string& path){
+    try{
+        if (!fs::exists(path)) {
+            fs::create_directory(path);
+            cout << "Carpeta creada exitosamente.\n";
+        } else {
+            cerr << "La carpeta ya existe.\n";
+        }
+    }catch(exception& e){
+        cout << "Error: " << e.what() << endl;
+    }
+}
+
+//eliminar una carpeta
+void eliminarCarpeta(const string& filePath){
+    try{
+        fs::remove_all(filePath);
+        cout << "Carpeta borrada exitosamente.\n";
+    }catch(exception& e){
+        cout << "Error: " << e.what() << endl;
+    }
+}
+
--- main.cpp
+++ main.cpp
@@ -2,12 +2,132 @@
 // Created by mmora on 11/04/2024.
 //
 
-#include "iostream"
 #include "funciones.h"
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
-    hola();
-    return 0;
+    string currentDir = ".";
+
+    while(true){
+
+
+        if(!validarDirectorio(currentDir)){//se valida que el directorio actual sea valido
+            cout<<"El directorio "+currentDir+" no existe"<<endl;
+            currentDir="/";
+            cout<<"El directorio se cambio a la raiz '/'"<<endl;
+        }
+
+        cout << "\nDirectorio actual: " << currentDir << "\n";
+        mostrarContenido(currentDir);
+
+        cout << "\nOpciones:\n";
+        cout << "1. Cambiar directorio\n";//ok
+        cout << "2. Copiar archivo\n";//********problemas con los permisos de los archivos*******
+        cout << "3. Renombrar archivo\n";//ok
+        cout << "4. Mover archivo\n";//*********error en la funcion***********
+        cout << "5. Borrar archivo\n";//ok
+        cout << "6. Renombrar carpeta\n";//ok
+        cout << "7. Crear carpeta\n";//ok
+        cout << "8. Borrar una carpeta\n";//ok
+        cout << "9. Abrir directorio\n";//ok
+        cout << "10. Salir\n";//ok
+
+
+        cout << "\nIngrese la opcion deseada: ";
+        int opcion;
+        cin >> opcion;
+
+        switch (opcion){
+            case 1: {//cambiar el directorio
+                string newDir;
+                cout << "Ingrese la ruta del nuevo directorio: ";
+                cin >> newDir;
+                currentDir = newDir;
+                break;
+            }
+            case 2:{//copiar archivos
+                string source, destination;
+                cout << "Ingrese el nombre del archivo a copiar: ";
+                cin >> source;
+                cout << "Copiar "+currentDir+"/"+source+" al directorio: ";
+                cin >> destination;
+                copiarArchivo(currentDir+"/"+source, destination);
+                break;
+            }
+            case 3:{//renombrar archivos
+                string oldName, newName;
+                cout << "Ingrese el nombre actual del archivo: ";
+                cin >> oldName;
+                cout << "Ingrese el nuevo nombre para el archivo: ";
+                cin >> newName;
+                renombrarArchivo(currentDir + "/" + oldName, currentDir + "/" + newName);
+                break;
+            }
+            case 4:{//mover un archivo
+                std::string source, destination;
+                std::cout << "Ingrese el nombre del archivo a mover: ";
+                std::cin >> source;
+                std::cout << "Ingrese la ruta de destino para mover el archivo: ";
+                std::cin >> destination;
+                moverArchivo(currentDir+"/"+source, destination);
+                break;
+            }
+            case 5:{//borrar un archivo
+                std::string filePath;
+                std::cout << "Ingrese el nombre del archivo a borrar: ";
+                std::cin >> filePath;
+                borrarArchivo(currentDir+"/"+filePath);
+                break;
+            }
+            case 6:{//cambiar el nombre a una carpeta
+                string oldName, newName;
+                cout << "Ingrese el nombre de la carpeta a renombrar: ";
+                cin >> oldName;
+                cout << "Ingrese el nuevo nombre para la carpeta: ";
+                cin >> newName;
+                renombrarCarpeta(currentDir + "/" + oldName, currentDir + "/" + newName);
+                break;
+            }
+            case 7:{//crear una carpeta
+                string newFolder;
+                cout << "Ingrese el nombre de la nueva carpeta: ";
+                cin >> newFolder;
+                crearCarpeta(currentDir + "/" + newFolder);
+                break;
+            }
+
+            case 8:{//eliminar una carpeta
+                string folder;
+                cout<<"ingrese el nombre de la carpeta a eliminar: " ;
+                cin>>folder;
+                eliminarCarpeta(currentDir+"/"+folder);
+                break;
+            }
+
+            case 9:{//abrir una carpeta del directorio actual
+                string newFolder;
+                cout<<"Ingrese el nombre de la carpeta que quiere abrir: ";
+                cin>>newFolder;
+
+                if(currentDir=="/"){
+                    currentDir=currentDir+newFolder;
+                }else{
+                    currentDir=currentDir+"/"+newFolder;
+                }
+                break;
+            }
+
+            case 10:
+                std::cout << "Saliendo del programa...\n";
+                return 0;
+
+            default:{
+                std::cerr << "Opcion invalida. Por favor, intente de nuevo.\n";
+                break;
+            }
+        }
+   }
 }
